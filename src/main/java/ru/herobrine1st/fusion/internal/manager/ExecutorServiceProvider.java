4381e82aee50e85fcd7335c21eee623c93a10b77
HeroBrine1st Erquilenne <herobrine1st@pm.me>
2021-08-12 11:28:24 +0300

    private ExecutorServiceProvider()

--- src/main/java/ru/herobrine1st/fusion/internal/manager/ExecutorServiceProvider.java
+++ src/main/java/ru/herobrine1st/fusion/internal/manager/ExecutorServiceProvider.java
@@ -3,7 +3,9 @@ package ru.herobrine1st.fusion.internal.manager;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
-public class ExecutorServiceProvider {
+public final class ExecutorServiceProvider {
+    private ExecutorServiceProvider() {}
+
     private static final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(2, it -> {
         var t = new Thread(it);
         t.setDaemon(true);
