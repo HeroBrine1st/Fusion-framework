0cb4c472fcd531338e86a514d152a922c34eabe5
HeroBrine1st Erquilenne <herobrine1st@pm.me>
2021-08-13 09:23:17 +0300

    Integer choices in range -2^32..2^32-1

--- src/main/java/ru/herobrine1st/fusion/api/command/args/parser/IntegerParserElement.java
+++ src/main/java/ru/herobrine1st/fusion/api/command/args/parser/IntegerParserElement.java
@@ -9,9 +9,11 @@ import ru.herobrine1st.fusion.api.command.CommandContext;
 import ru.herobrine1st.fusion.api.command.args.CommandArgs;
 import ru.herobrine1st.fusion.api.exception.ArgumentParseException;
 
+import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
-public class IntegerParserElement extends ParserElement<IntegerParserElement, Long> {
+public class IntegerParserElement extends ChoicesParserElement<IntegerParserElement, Long> {
 
     private final long min;
     private final long max;
@@ -26,14 +28,24 @@ public class IntegerParserElement extends ParserElement<IntegerParserElement, Lo
 
     @Override
     public Long parseValue(@NotNull CommandArgs args, CommandContext ctx) throws ArgumentParseException {
-        long res;
-        try {
-            res = Long.parseLong(args.next().getValue(), radix);
-        } catch(NumberFormatException e) {
-            throw ArgumentParseException.withPointer("Неправильный формат числа", args);
+        if(choices.isEmpty()) {
+            long res;
+            try {
+                res = Long.parseLong(args.next().getValue(), radix);
+            } catch (NumberFormatException e) {
+                throw ArgumentParseException.withPointer("Illegal format number", args);
+            }
+            if (res < min || res > max)
+                throw ArgumentParseException.withPointer("Integer is out of range %s..%s".formatted(min, max), args);
+            return res;
+        } else {
+            Long value = choices.get(args.next().getValue());
+            if (value == null) {
+                throw ArgumentParseException.withPointer("Argument doesn't fit the choices: " +
+                        choices.keySet().stream().map(it -> "\"" + it + "\"").collect(Collectors.joining(", ")), args);
+            }
+            return value;
         }
-        if(res < min || res > max) throw ArgumentParseException.withPointer("Число выходит за предел %s..%s".formatted(min, max), args);
-        return res;
     }
 
     @Override
@@ -43,7 +55,11 @@ public class IntegerParserElement extends ParserElement<IntegerParserElement, Lo
 
     @Override
     public OptionData getOptionData() {
-        return new OptionData(OptionType.INTEGER, name, description, required);
+        OptionData optionData = new OptionData(OptionType.INTEGER, name, description, required);
+        for(Map.Entry<String, Long> entry: choices.entrySet()) {
+            optionData.addChoice(entry.getKey(), entry.getValue().intValue()); // FIXME range -2^32..2^32-1; Waiting library fix
+        }
+        return optionData;
     }
 
     @Override
@@ -51,12 +67,14 @@ public class IntegerParserElement extends ParserElement<IntegerParserElement, Lo
         OptionMapping option = interaction.getOption(name);
         if(option == null) throw new NoSuchElementException();
         long res = option.getAsLong();
-        if(res < min || res > max) throw new ArgumentParseException("Аргумент %S выходит за предел %s..%s".formatted(name, min, max));
+        if(res < min || res > max) throw new ArgumentParseException("Integer %S is out of range %s..%s".formatted(name, min, max));
         return res;
     }
 
     @Override
     public String getRawUsage() {
-        return "integer";
+        if(choices.isEmpty())
+            return "integer";
+        else return String.join("|", choices.keySet());
     }
 }
