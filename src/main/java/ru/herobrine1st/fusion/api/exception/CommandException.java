7efcd228621a86b342d5c3b83d3df147c5fa0956
HeroBrine1st Erquilenne <herobrine1.videos@gmail.com>
2021-11-03 11:00:10 +0300

    Make CommandException extend RuntimeException

--- src/main/java/ru/herobrine1st/fusion/api/exception/CommandException.java
+++ src/main/java/ru/herobrine1st/fusion/api/exception/CommandException.java
@@ -1,6 +1,6 @@
 package ru.herobrine1st.fusion.api.exception;
 
-public class CommandException extends Exception {
+public class CommandException extends RuntimeException {
     public CommandException(String message) {
         super(message);
     }
